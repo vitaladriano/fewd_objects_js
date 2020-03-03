07bcfa64d39246104efbc59ee8ae231c8a7db7e7
vitaladriano <adriano.mirandav@hotmail.com>
2020-03-03 11:41:24 +0000

    commit

--- js/script.js
+++ js/script.js
@@ -1,46 +1,41 @@
-var hotel = {
+var club = {
     // Object properties
-    name: 'Clayton',
-    rooms: 110,
-    booked: 25,
-    gym: true,
-    roomTypes: ['double', 'twin', 'suite', 'king'],
+    name: 'Folk Room Club',
+    tickets: 500,
+    sold: 455,
+    ticketsTypes: ['VIP', 'seating', 'Early bird', 'standing'],
     checkAvailablity: function() {
-        return this.rooms - this.booked;
+        return this.tickets - this.sold;
     }
 };
 
-var elName = document.getElementById('hotelName');
-elName.textContent = hotel.name;
+var elName = document.getElementById('clubName');
+elName.textContent = club.name;
 
-var elRooms = document.getElementById('rooms');
-elRooms.textContent = hotel.checkAvailablity();
+var eltickets = document.getElementById('tickets');
+eltickets.textContent = club.checkAvailablity();
 
 // Define a constructor function template for hotels
-function Hotel(name, rooms, booked) {
+function Club(name, tickets, sold) {
     // Object properties
     this.name = name;
-    this.rooms = rooms;
-    this.booked = booked;
+    this.tickets = tickets;
+    this.sold = sold;
     this.checkAvailablity = function() {
-        return this.rooms - this.booked;
+        return this.tickets - this.sold;
     };
 }
 
 // Define to instances of the hotel and their arguments
-var clayHotel = new Hotel('Clayton', 68, 3);
-var lemonHotel = new Hotel('Lemon', 56, 15);
+var concert1 = new Club('Jhonny Cash Cover', 450, 425);
+var concert2 = new Club('Iron and Wine', 450, 400);
 
-// Declare a variable to hold the name of the hotel and room availability 
-var details1 = clayHotel.name + ' rooms: ';
-    // add content to an existing variable----
-    details1 += clayHotel.checkAvailablity();
-// Get the hotel1 div
-var elHotel1 = document.getElementById('hotel1');
-// Add the variables created onto the empty div
-elHotel1.textContent = details1;
+var details1 = concert1.name + ' tickets: ';
+    details1 += concert1.checkAvailablity();
+var elconcert1 = document.getElementById('concert1');
+elconcert1.textContent = details1;
 
-var details2 = lemonHotel.name + ' rooms: ';
-    details2 += lemonHotel.checkAvailablity();
-var elHotel2 = document.getElementById('hotel2');
-elHotel2.textContent = details2;
+var details2 = concert2.name + ' tickets: ';
+    details2 += concert2.checkAvailablity();
+var elconcert2 = document.getElementById('concert2');
+elconcert2.textContent = details2;
